e614eb710cf40992b1e70794eae962eb6c63b414
Ahmed <ahmed.mohamed547@gmail.com>
2024-07-05 15:26:40 +0000

    enable micro theme

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,3 +1,87 @@
+{ pkgs, config, ... }:
 { 
-    
+  programs.fd = {             # Enable fd and set defaults
+  	enable = true;
+  	hidden = true;
+  };
+  programs.bat = {            # Enable bat and set defaults
+  	enable = true;
+  };
+  programs.fzf = {            # Enable fzf and set defaults
+  	enable = true;
+  	enableZshIntegration = true;
+  	defaultCommand = "fd -t d . $HOME";
+  	defaultOptions = [ "--preview 'bat --color=always {}'" ];
+  	fileWidgetCommand = "fd -t d . $HOME";
+  };
+  programs.eza = {            # Enable eza and set defaults
+  	enable = true;
+  	enableZshIntegration = true;
+  	extraOptions = [ "--group-directories-first" "--icons" "--git"];
+  };
+  programs.zoxide = {          # Enable zoxide and set defaults
+  	enable = true;
+  	enableZshIntegration = true;
+  	options = [ "--cmd cd"];
+  };
+  programs.micro = {           # Enable micro and set defaults
+  	enable = true;
+  	settings = {
+  		colorscheme = "default";
+  	};
+  };
+  programs.thefuck = {
+  	enable = true;
+  	enableZshIntegration = true;
+  };
+
+## ZSH settings #######################################################################
+  programs.zsh = {
+    enable = true;
+    zplug = {
+    	enable = true;
+    	plugins = [
+    		{ name = "zsh-users/zsh-autosuggestions"; }
+    		{ name = "zdharma-continuum/fast-syntax-highlighting"; }
+    		{ name = "Aloxaf/fzf-tab"; }
+    		{ name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; }	
+    	];
+    };
+    enableCompletion = true;
+    history = {
+          ignoreDups = true;
+          ignoreSpace = true;
+          save = 10000;
+          size = 10000;
+    };
+    defaultKeymap = "emacs";
+    #  Aliases  ##########################
+    shellAliases = {
+        lsd = "eza --icons --tree --dir-only --git-repos --only-dirs --level=3";
+        update = "sudo nixos-rebuild switch";
+        hs = "home-manager switch";
+        c="clear";
+        lsa="eza --icons -a -l";
+        ssh = "ssh.exe";
+
+    };
+    initExtra = ''
+          [[ ! -f ${./modules/zsh/.p10k.zsh} ]] || source ${./modules/zsh/.p10k.zsh}
+          [[ ! -e op.exe ]] || alias ssh="ssh.exe"
+          bindkey '^p' history-search-backward
+          bindkey '^n' history-search-forward
+
+        '';
+  };
+## Environmental varialbles  ##########################################################
+  home.sessionVariables = {
+    EDITOR = "micro";
+    FZF_COMPLETION_TRIGGER= "==";
+    MICRO_TRUECOLOR=1;
+  };
+
+## Dot file Management  ###############################################################
+  home.file = {
+    "~/.config/micro/colorschemes/catppuccin-mocha.micro".source = config.lib.file.mkOutOfStoreSymlink /modules/src/micro/catppuccin-mocha.micro;
+  };
 }
