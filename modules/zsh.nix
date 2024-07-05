0b7b5a88a24e7762df90d86885cb3505598d0bd5
Ahmed <ahmed.mohamed547@gmail.com>
2024-07-05 19:12:37 +0000

    changed plugin manager - zinit && added aliases

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,5 +1,8 @@
 { pkgs, config, ... }:
 
+let
+  ZINIT_HOME = "$HOME/.local/share/zinit/zinit.git";
+in
 { 
   programs.fd = {             # Enable fd and set defaults
   	enable = true;
@@ -35,52 +38,74 @@
   	enable = true;
   	enableZshIntegration = true;
   };
+  home.packages = [
+    pkgs.zinit
+  ];
 
 #----- ZSH management ----------------------------------------------------------#
   programs.zsh = {
     enable = true;
-    zplug = {
-    	enable = true;
-    	plugins = [
-    		{ name = "zsh-users/zsh-autosuggestions"; }
-    		{ name = "zdharma-continuum/fast-syntax-highlighting"; }
-    		{ name = "Aloxaf/fzf-tab"; }
-    		{ name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; }
-
-    	];
-    };
-    enableCompletion = true;
+    #enableCompletion = true;
     history = {
+        extended = true;
+        share = true;
         ignoreDups = true;
         ignoreSpace = true;
+        expireDuplicatesFirst = true;
         save = 10000;
         size = 10000;
     };
     defaultKeymap = "emacs";
     #----- Aliases -------------------------------------------------------------#
     shellAliases = {
-        lsd = "eza --icons --tree --dir-only --git-repos --only-dirs --level=3";
+        lsd = "eza --icons --tree -D --git-repos --level=3";
         update = "sudo nixos-rebuild switch";
         hs = "home-manager switch";
         c="clear";
         lsa="eza --icons -a -l";
+        lst = "eza --icons --tree --level=2";
+        eh = "code ~/dotfiles/home.nix";
+        es = "code ~/dotfiles/system/configuration.nix";
         ssh = "ssh.exe";
+        dothome = "home-manager switch --flake .#nixos";
+        dotsys = "nixos-rebuild boot -I nixos-config=system/configuration.nix";
 
     };
+    # Settings to be added to the top of .zshrc
+    initExtraFirst = ''
+    '';
+    initExtraBeforeCompInit = ''
+    '';
     initExtra = ''
-        [[ ! -f ${./src/zsh/.p10k.zsh} ]] || source ${./src/zsh/.p10k.zsh}
+        source ${pkgs.zinit}/share/zinit/zinit.zsh
+        zinit ice depth=1; zinit light romkatv/powerlevel10k    # Install powerlevel10k
+        zinit light zdharma-continuum/fast-syntax-highlighting
+        zinit light zsh-users/zsh-completions
+        zinit light zsh-users/zsh-autosuggestions
+        zinit light Aloxaf/fzf-tab
+        zinit snippet OMZP::git
+        zinit snippet OMZP::sudo
+        zinit snippet OMZP::command-not-found
+        zinit snippet OMZP::nvm
+        zinit snippet OMZP::colored-man-pages
+        export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+
         [[ ! -e op.exe ]] || alias ssh="ssh.exe"
+
         bindkey '^p' history-search-backward
         bindkey '^n' history-search-forward
 
+        [[ ! -f ${./src/zsh/.p10k.zsh} ]] || source ${./src/zsh/.p10k.zsh}
+
         '';
   };
-  
+
 #----- Environmental variables -------------------------------------------------#
   home.sessionVariables = {
     EDITOR = "micro";
     FZF_COMPLETION_TRIGGER= "==";
     MICRO_TRUECOLOR=1;
+
   };
 
 #----- Dotfile management ------------------------------------------------------#
