b81bb6b43d6d6a834b32a04a520240151a886bfb
Ahmed <ahmed.mohamed547@gmail.com>
2024-07-07 20:37:19 +0000

    improve direnv config management

--- modules/direnv.nix
+++ modules/direnv.nix
@@ -5,6 +5,9 @@
       enable = true;
       enableZshIntegration = true;
       nix-direnv.enable = true;
-      silent = true;
+      config = {
+        global = { hide_env_diff = true; };    # Hid the diff of the environment variables when direnv is loaded
+        whitelist = { prefix = [ "~/repos" ]; };   # Direnv will autoload in these directories
+      };
     };
   }
