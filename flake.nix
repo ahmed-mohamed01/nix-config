c5407338290d3835e914b328d628fad2f5b81beb
Ahmed <ahmed.mohamed547@gmail.com>
2024-07-08 16:00:52 +0000

    added hosts and multiple device configs to flake.nix. WIP

--- flake.nix
+++ flake.nix
@@ -3,16 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";   # Change this to 'nixos-24.05' for a stable release.
-
-    #----- Other custom inputs ---------------------------------#
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";    # Enable if using NixOS-WSL
-    #_1password-shell-plugins.url = "github:1Password/shell-plugins";   # Enable if using 1password shell plugins
-    catppuccin.url = "github:catppuccin/nix";     # Enable if using catppuccin's home-manager modules
-
-    home-manager = {
+    home-manager = {    # Enable home-manager input
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    #----- Other custom inputs ---------------------------------------------------------------------------------#
+
+    nixos-wsl = {    # Enable if using NixOS-WSL
+      url = "github:nix-community/NixOS-WSL/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };    
+    #_1password-shell-plugins.url = "github:1Password/shell-plugins";   # Enable if using 1password shell plugins
+    catppuccin.url = "github:catppuccin/nix";     # Enable if using catppuccin's home-manager modules
+
   };
                                
   outputs = { self, nixpkgs, catppuccin, nixos-wsl, home-manager, ... }@inputs: 
@@ -20,6 +23,7 @@
       lib = nixpkgs.lib;
       system = "x86_64-linux";    # Change this to "x86_64-darwin" for macOS
       pkgs = import nixpkgs { inherit system; };
+      inherit (self) outputs;
     in {
       #imports = [ inputs._1password-shell-plugins.hmModules.default ];   # Enable if using 1password shell plugins
 
@@ -31,10 +35,7 @@
         spectre = nixpkgs.lib.nixosSystem {
           inherit pkgs;
           specialArgs = {inherit inputs outputs;};
-          modules = [
-            ./hosts/spectre/configuration.nix
-            
-            ];
+          modules = [ ./hosts/spectre/configuration.nix ];
         };
 
         #----- WSL configuration ------------------------------------------------------------------------------#
@@ -42,39 +43,25 @@
           inherit pkgs;
           specialArgs = {inherit inputs outputs;};
           modules = [
-            nixos-wsl.nixosModules.default {
-              wsl.enable = true;
-            }
+            nixos-wsl.nixosModules.default { wsl.enable = true; }
             ./hosts/wsl/configuration.nix
           ];
         };
 
         #----- Virtual Box VM config --------------------------------------------------------------------------#
-        vm = nixos-wsl.nixosSystem {
+        vm = nixpkgs.lib.nixosSystem {
           inherit pkgs;
           specialArgs = {inherit inputs outputs;};
-          modules = [
-            nixos-wsl.nixosModules.default {
-              system.stateVersion = "24.05";
-              wsl.enable = true;
-            }
-            ./hosts/vm/configuration.nix
-          ];
+          modules = [ ./hosts/vm/configuration.nix ];
         };
 
         #----- Main desktop configuration ---------------------------------------------------------------------#
-        um790 = nixos-wsl.nixosSystem {
+        um790 = nixpkgs.lib.nixosSystem {
           inherit pkgs;
           specialArgs = {inherit inputs outputs;};
-          modules = [
-            nixos-wsl.nixosModules.default {
-              system.stateVersion = "24.05";
-              wsl.enable = true;
-            }
-            ./hosts/um790/configuration.nix
-          ];
-        
+          modules = [ ./hosts/um790/configuration.nix ];
         };
+      };
 
   #----- Home manbager configurations, identified via hostname ------------------------------------------------#
     #---- Available through 'home-manager --flake .#your-username@your-hostname' ------------------------------#
@@ -86,20 +73,16 @@
           modules = [ 
             ./home.nix
             catppuccin.homeManagerModules.catppuccin
-            
             ];
         };
        #---- WSL config, managed via home-manager -------------------------------------------------------------#
         wsl = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [ 
-            ./hosts/wsl/home.nix
+            ./hosts/wsl/home.nix    # WSL specific home-manager settings, incl git, 1password, etc.
             catppuccin.homeManagerModules.catppuccin
             ];
         };
-
       };
-
-  };
-  
+  };  
 }
