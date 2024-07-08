104ba7b6f2eb04411841bcbb83e4202aa9546888
Ahmed <ahmed.mohamed547@gmail.com>
2024-07-08 18:13:13 +0000

    modified flake, does not work

--- flake.nix
+++ flake.nix
@@ -7,8 +7,8 @@
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    #----- Other custom inputs ---------------------------------------------------------------------------------#
 
+    #----- Other custom inputs ---------------------------------------------------------------------------------#
     nixos-wsl = {    # Enable if using NixOS-WSL
       url = "github:nix-community/NixOS-WSL/main";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -18,7 +18,7 @@
 
   };
                                
-  outputs = { self, nixpkgs, catppuccin, nixos-wsl, home-manager, ... }@inputs: 
+  outputs = { self, nixpkgs, catppuccin, nixos-wsl, home-manager, ... }@inputs:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";    # Change this to "x86_64-darwin" for macOS
